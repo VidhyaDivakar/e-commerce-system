97b8278924eaecce1529db3ce6f66cb23eeb4109
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-04-09 01:04:45 -0400

    updated the taxcalculater.ts

--- src/utils/taxCalculator.ts
+++ src/utils/taxCalculator.ts
@@ -1,1 +1,7 @@
-calculateTax() // unction to handle tax calculations for products.
+import { Product } from "../models/Product";
+export function calculateTax(product: Product): number {
+    const taxRate = 0.0475;   // default tax rate of 4.75%  
+    const groceryTaxrate = 0.03
+    const taxCharge = product.category.toLowerCase() === "groceries" ? groceryTaxrate : taxRate; // use of ternary operator to check if the category is groceries and apply the appropriate tax rate. its like if else statement but more concise. if the condition is true it will return groceryTaxrate otherwise it will return taxRate.
+    return product.price * taxCharge;
+}
